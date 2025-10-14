9f792f75cdc3a9069e85cc6ece9a55fe05e3c804
mayli4 <sydsleeper@gmail.com>
2025-10-13 23:15:57 -0400

    wip: init gut of cthulhu stubs

---
+++ Content/NPCs/Bosses/GutOfCthulhu/GutOfCthulhu.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityVanilla.Content.NPCs.Bosses.GutOfCthulhu;
+
+internal enum GutAttack
+{
+    Idle
+}
+
+internal sealed class GutOfCthulhu : ModNPC
+{
+    public override string Texture => "CalamityVanilla/Content/NPCs/Bosses/GutOfCthulhu/GutOfCthulhu_Body";
+
+    public override void SetDefaults()
+    {
+        (NPC.width, NPC.height) = (200, 200);
+        
+        NPC.lifeMax = 100;
+        NPC.defense = 30;
+
+        NPC.aiStyle = -1;
+        NPC.noGravity = true;
+        NPC.noTileCollide = true;
+
+        NPC.HitSound = ContentSamples.NpcsByNetId[NPCID.IceElemental].HitSound;
+        NPC.DeathSound = ContentSamples.NpcsByNetId[NPCID.IceElemental].DeathSound;
+    }
+
+    public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+    {
+        return true;
+    }
+}
