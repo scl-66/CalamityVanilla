c0530a250c3d7e9694c2bb3d219066b40dbe8832
mayli4 <sydsleeper@gmail.com>
2025-10-15 03:15:29 -0400

    feat: astro world flag

---
+++ Common/World/CommonWorldFlags.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace CalamityVanilla.Common.World;
+
+internal sealed class CommonWorldFlags : ModSystem
+{
+    public static bool AstroOrHallow;
+    
+    #if DEBUG
+    public override void PostUpdateEverything()
+    {
+        if (Main.keyState.IsKeyDown(Keys.L) && !Main.oldKeyState.IsKeyDown(Keys.L))
+        {
+            AstroOrHallow = !AstroOrHallow;
+
+            string status = AstroOrHallow ? "enabled" : "disabled";
+            Main.NewText($"Astro/Hallow flag is now {status}.");
+        }
+        
+        if (Main.keyState.IsKeyDown(Keys.K) && !Main.oldKeyState.IsKeyDown(Keys.K))
+        {
+            string status = AstroOrHallow ? "enabled" : "disabled";
+            Main.NewText($"Astro/Hallow flag is {status}.");
+        }
+    }
+#endif
+    
+    public override void OnWorldLoad()
+    {
+        AstroOrHallow = false;
+    }
+    
+    public override void OnWorldUnload() 
+    {
+        AstroOrHallow = false;
+    }
+    
+    public override void SaveWorldData(TagCompound tag)
+    {
+        tag["CalamityVanilla:HasAstro"] = AstroOrHallow; 
+    }
+    
+    public override void SaveWorldHeader(TagCompound tag)
+    {
+        tag["CalamityVanilla:HasAstro"] = AstroOrHallow;
+    }
+
+    public override void LoadWorldData(TagCompound tag)
+    {
+        AstroOrHallow = tag.GetBool("CalamityVanilla:HasAstro");
+    }
+}
