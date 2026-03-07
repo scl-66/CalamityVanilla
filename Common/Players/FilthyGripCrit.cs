c750f1d50c5455fbd7db4ae55cec4b57100f2ad2
sixtydegrees <67486775+scl-66@users.noreply.github.com>
2026-03-07 02:34:43 -0500

    wip: filthy grip (proj not damaging rn)

---
+++ Common/Players/FilthyGripCrit.cs
@@ -0,0 +1,26 @@
+﻿using CalamityVanilla.Content.Bosses.GutOfCthulhu.Drops.ParasiticBelt;
+using CalamityVanilla.Content.Bosses.HiveMind.Drops.FilthyGrip;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityVanilla.Common.Players;
+
+public class FilthyGripCrit : ModPlayer
+{
+    public bool filthyGripEquipped = false;
+    public override void ResetEffects()
+    {
+        filthyGripEquipped = false;
+    }
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+    {
+        if (hit.Crit && filthyGripEquipped)
+        {
+            Projectile.NewProjectile(Player.GetSource_FromThis(), target.position + new Vector2(0, -5f), Vector2.Zero, ModContent.ProjectileType<FilthyGripSlash>(), 30, 0f, Main.myPlayer);
+        }
+    }
+}
---
+++ Content/Bosses/HiveMind/Drops/FilthyGrip/FilthyGrip.cs
@@ -0,0 +1,53 @@
+﻿using CalamityVanilla.Common.Players;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityVanilla.Content.Bosses.HiveMind.Drops.FilthyGrip;
+
+[AutoloadEquip(EquipType.HandsOn)]
+public class FilthyGrip : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.width = 16;
+        Item.height = 16;
+        Item.maxStack = 1;
+        Item.value = 65000;
+        Item.rare = ItemRarityID.Pink;
+        Item.accessory = true;
+        // Set other Item.X values here
+    }
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        player.GetModPlayer<FilthyGripCrit>().filthyGripEquipped = true;
+    }
+}
+
+public class FilthyGripSlash : ModProjectile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.projFrames[Type] = 7;
+    }
+    public override void SetDefaults()
+    {
+        Projectile.width = Projectile.height = 11;
+        Projectile.friendly = true;
+        Projectile.timeLeft = 21;
+        Projectile.usesLocalNPCImmunity = true;
+        Projectile.localNPCHitCooldown = -1;
+    }
+    public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+    {
+        modifiers.ScalingArmorPenetration += 1f;
+    }
+    public override void AI()
+    {
+        if (++Projectile.frameCounter >= 3)
+        {
+            Projectile.frameCounter = 0;
+            Projectile.frame = ++Projectile.frame % Main.projFrames[Projectile.type];
+        }
+    }
+}
