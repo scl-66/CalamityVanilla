f12bf9df035dbb3ac8b6f5d2c4c75c2ad2e79740
sixtydegrees <67486775+scl-66@users.noreply.github.com>
2026-03-21 20:29:09 -0400

    radioactive ores, irratiated debuff, working on death msg

---
+++ Content/Buffs/Debuffs/IrradiatedDebuff.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace CalamityVanilla.Content.Buffs.Debuffs;
+
+// This class serves as an example of a debuff that causes constant loss of life
+// See ExampleLifeRegenDebuffPlayer.UpdateBadLifeRegen at the end of the file for more information
+public class IrradiatedDebuff : ModBuff
+{
+    public override void SetStaticDefaults()
+    {
+        Main.debuff[Type] = true;  // Is it a debuff?
+        Main.pvpBuff[Type] = true; // Players can give other players buffs, which are listed as pvpBuff
+        Main.buffNoSave[Type] = true; // Causes this buff not to persist when exiting and rejoining the world
+        BuffID.Sets.LongerExpertDebuff[Type] = true; // If this buff is a debuff, setting this to true will make this buff last twice as long on players in expert mode
+    }
+
+    // Allows you to make this buff give certain effects to the given player
+    public override void Update(Player player, ref int buffIndex)
+    {
+        player.GetModPlayer<IrradiatedRegen>().lifeRegenDebuff = true;
+    }
+}
+
+public class IrradiatedRegen : ModPlayer
+{
+    // Flag checking when life regen debuff should be activated
+    public bool lifeRegenDebuff;
+    public int damage;
+
+    public override void ResetEffects()
+    {
+        lifeRegenDebuff = false;
+    }
+
+    // Allows you to give the player a negative life regeneration based on its state (for example, the "On Fire!" debuff makes the player take damage-over-time)
+    // This is typically done by setting player.lifeRegen to 0 if it is positive, setting player.lifeRegenTime to 0, and subtracting a number from player.lifeRegen
+    // The player will take damage at a rate of half the number you subtract per second
+    public override void UpdateBadLifeRegen()
+    {
+        if (lifeRegenDebuff)
+        {
+            // These lines zero out any positive lifeRegen. This is expected for all bad life regeneration effects
+            if (Player.lifeRegen > 0)
+                Player.lifeRegen = 0;
+            // Player.lifeRegenTime used to increase the speed at which the player reaches its maximum natural life regeneration
+            // So we set it to 0, and while this debuff is active, it never reaches it
+            Player.lifeRegenTime = 0;
+            // lifeRegen is measured in 1/2 life per second. Therefore, this effect causes 8 life lost per second
+            Player.lifeRegen -= damage;
+        }
+    }
+    public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genDust, ref PlayerDeathReason damageSource)
+    {
+        if (lifeRegenDebuff)
+        {
+            damageSource = PlayerDeathReason.ByCustomReason(NetworkText.FromKey(Language.GetTextValue("Mods.CalamityVanilla.DeathMessage.RadiationPoisoned1").ToNetworkText(Player.name)));
+        }
+        return true;
+    }
+}
---
+++ Content/Underground/Items/Ores/PostHardmode/RadioactiveOre.cs
@@ -0,0 +1,91 @@
+﻿using CalamityVanilla.Content.Buffs.Debuffs;
+using CalamityVanilla.Content.Rarities;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace CalamityVanilla.Content.Underground.Items.Ores.PostHardmode;
+
+public class UraniumOre : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.DefaultToPlaceableTile(ModContent.TileType<UraniumOreTile>(), 0);
+        Item.value = 2050;
+        Item.rare = ModContent.RarityType<CobaltRarity>();
+    }
+}
+public class PlutoniumOre : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.DefaultToPlaceableTile(ModContent.TileType<PlutoniumOreTile>(), 0);
+        Item.value = 2200;
+        Item.rare = ModContent.RarityType<CobaltRarity>();
+    }
+}
+
+public class UraniumOreTile : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileSolid[Type] = true;
+        Main.tileMergeDirt[Type] = true;
+        Main.tileBlockLight[Type] = true;
+        Main.tileLighted[Type] = true;
+        MinPick = 225;
+        MineResist = 5f;
+
+        TileID.Sets.Ore[Type] = true;
+        TileID.Sets.FriendlyFairyCanLureTo[Type] = true;
+        Main.tileSpelunker[Type] = true; // The tile will be affected by spelunker highlighting
+        Main.tileOreFinderPriority[Type] = 850; // Metal Detector value, see https://terraria.wiki.gg/wiki/Metal_Detector
+        Main.tileShine2[Type] = true; // Modifies the draw color slightly.
+        Main.tileShine[Type] = 975; // How often tiny dust appear off this tile. Larger is less frequently
+
+
+        LocalizedText name = CreateMapEntryName();
+        AddMapEntry(new Color(226, 246, 87), name);
+
+        DustType = DustID.Chlorophyte;
+        VanillaFallbackOnModDeletion = TileID.LunarOre;
+        HitSound = SoundID.Tink;
+    }
+
+    public override void NearbyEffects(int i, int j, bool closer)
+    {
+        float dist = Vector2.Distance(Main.LocalPlayer.position, new Vector2(i * 16f, j * 16f));
+        if (dist < 100f)
+        {
+            Main.LocalPlayer.AddBuff(ModContent.BuffType<IrradiatedDebuff>(), 5);
+            Main.LocalPlayer.GetModPlayer<IrradiatedRegen>().damage = (int)(Math.Pow(1 / dist * 300, 1.4)); //placeholder damage value
+        }
+    }
+
+    public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+    {
+        r = 0.6f/3f;
+        g = 0.85f/3f;
+        b = 0.05f/3f;
+    }
+}
+
+public class PlutoniumOreTile : UraniumOreTile {
+    public new void SetStaticDefaults()
+    {
+        Main.tileOreFinderPriority[Type] = 860; // Metal Detector value, see https://terraria.wiki.gg/wiki/Metal_Detector
+        LocalizedText name = CreateMapEntryName();
+        AddMapEntry(new Color(232, 186, 86), name);
+
+        DustType = DustID.Palladium;
+    }
+    public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+    {
+        r = 0.9f / 3f;
+        g = 0.6f / 3f;
+        b = 0.3f / 3f;
+    }
+}
