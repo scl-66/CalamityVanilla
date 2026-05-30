e1e2b690915c46b1042b784ca8b0be617ab19893
sixtydegrees <67486775+scl-66@users.noreply.github.com>
2026-05-30 18:27:23 -0400

    fruit punch progress

--- Content/NPCs/TownNPCs/Priest/Items/FruitPunchBowl.cs
+++ Content/NPCs/TownNPCs/Priest/Items/FruitPunchBowl.cs
@@ -1,26 +1,37 @@
 ﻿using CalamityVanilla.Content.Dusts;
+using Daybreak.Common.Rendering;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.Enums;
+using Terraria.GameContent.UI;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
+using static Terraria.GameContent.Animations.IL_Actions.Sprites;
 
 namespace CalamityVanilla.Content.NPCs.TownNPCs.Priest.Items;
 
-
-public class FruitPunchBowlTile : ModTile
+public class FruitPunchBowlEntity : ModTileEntity
 {
-    public static string[] potionNameContainsList = { "Bottled", "Potion", "Juice", "Brew", "Flask" };
-    public static string[] potionNameBlacklist = { "Statue", "Decorative" };
-    public Player player;
-    public bool heldItemcontainsPotionName;
     public int potionType;
     public bool filled;
+    public Color juiceColor;
+    public override bool IsTileValidForEntity(int x, int y)
+    {
+        Tile tile = Main.tile[x, y];
+        //The MyTile class is shown later
+        return tile.HasTile && tile.TileType == ModContent.TileType<FruitPunchBowlTile>();
+    }
+}
+public class FruitPunchBowlTile : ModTile
+{
+    public bool heldItemIsPotionable;
 
     public override void SetStaticDefaults()
     {
@@ -32,37 +43,108 @@ public class FruitPunchBowlTile : ModTile
         Main.tileObsidianKill[Type] = true;
         TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
         TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.Table, TileObjectData.Style2x1.Width, 0);
+        TileObjectData.newTile.HookPostPlaceMyPlayer = ModContent.GetInstance<FruitPunchBowlEntity>().Generic_HookPostPlaceMyPlayer;
         TileObjectData.addTile(Type);
+    }
+
+    public override void KillMultiTile(int i, int j, int frameX, int frameY)
+    {
+        ModContent.GetInstance<FruitPunchBowlEntity>().Kill(i, j);
     }
 
     public override void MouseOver(int i, int j)
     {
-        player = Main.LocalPlayer;
-        heldItemcontainsPotionName = potionNameContainsList.Any(player.HeldItem.Name.Contains) && !potionNameBlacklist.Any(player.HeldItem.Name.Contains);
-        potionType = player.HeldItem.netID;
-        player.cursorItemIconEnabled = true;
-        if (!heldItemcontainsPotionName)
+        if (TileEntity.TryGet(i, j, out FruitPunchBowlEntity entity))
         {
-            player.cursorItemIconID = ItemID.BottledWater;
-        } else
-        {
-            player.cursorItemIconID = potionType;
+            Player player = Main.LocalPlayer;
+            Item item = player.HeldItem;
+            bool hasBottleInRecipe = RecipeLoader.FirstRecipeForItem[item.type].HasIngredient(ItemID.BottledWater) || RecipeLoader.FirstRecipeForItem[item.type].HasIngredient(ItemID.Bottle);
+
+            heldItemIsPotionable = item.consumable & hasBottleInRecipe;
+            //Main.NewText(itemName);
+            entity.potionType = player.HeldItem.netID;
+            player.cursorItemIconEnabled = true;
+            if (!entity.filled)
+            {
+                if (!heldItemIsPotionable)
+                {
+                    player.cursorItemIconID = ItemID.BottledWater;
+                }
+                else
+                {
+                    player.cursorItemIconID = entity.potionType;
+                }
+            }
+            else
+            {
+                player.cursorItemIconID = ItemID.Bottle;
+            }
         }
     }
 
     public override bool RightClick(int i, int j)
     {
-        if (heldItemcontainsPotionName)
+        if (TileEntity.TryGet(i, j, out FruitPunchBowlEntity tileEntity) && !tileEntity.filled)
         {
-            //Main.NewText(player.HeldItem.Name);
-
-            SoundEngine.PlaySound(SoundID.SplashWeak with { MaxInstances = 0 }, new Vector2(i, j).ToWorldCoordinates());
-            return true;
+            if (heldItemIsPotionable)
+            {
+                //Main.NewText(player.HeldItem.Name);
+                Vector2 tileCoords = new Vector2(i, j).ToWorldCoordinates();
+                SoundEngine.PlaySound(SoundID.SplashWeak with { MaxInstances = 0 }, tileCoords);
+                for (int k = 0; k < Main.rand.Next(3, 7); k++)
+                {
+                    Dust d = Dust.NewDustDirect(tileCoords - new Vector2(0, 16), 16, 16, DustID.Water, 0, Main.rand.NextFloat(-5f, -1.5f));
+                    d.scale = Main.rand.NextFloat(0.5f, 0.75f);
+                }
+                tileEntity.filled = true;
+                return true;
+            }
+            else
+            {
+                //Main.NewText("does not has potion");
+                return false;
+            }
+        } else
+        {
+            if (!heldItemIsPotionable)
+            {
+                Vector2 tileCoords = new Vector2(i, j).ToWorldCoordinates();
+                SoundEngine.PlaySound(SoundID.Item3 with { MaxInstances = 0 }, tileCoords);
+                for (int k = 0; k < Main.rand.Next(3, 7); k++)
+                {
+                    Dust d = Dust.NewDustDirect(tileCoords - new Vector2(0, 16), 16, 16, DustID.Water, 0, Main.rand.NextFloat(-5f, -1.5f));
+                    d.scale = Main.rand.NextFloat(0.5f, 0.75f);
+                }
+                tileEntity.filled = false;
+                return true;
+            }
         }
-        else
+        return false;
+    }
+
+    public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+    {
+        if (TileEntity.TryGet(i, j, out FruitPunchBowlEntity entity) && entity.filled)
         {
-            //Main.NewText("does not has potion");
-            return true;
+            var tex = ModContent.Request<Texture2D>("CalamityVanilla/Content/NPCs/TownNPCs/Priest/Items/FruitPunchBowlTile_Juice").Value;
+            entity.juiceColor = ItemRarity.GetColor(ContentSamples.ItemsByType[entity.potionType].rare);
+            Tile tile = Framing.GetTileSafely(i, j);
+            int frameX = tile.TileFrameX;
+            int frameY = tile.TileFrameY;
+            Rectangle frame = new(frameX, frameY, tex.Width, tex.Height);
+
+            Vector2 offsetZero = Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange);
+
+            Vector2 location = new Vector2(i, j).ToWorldCoordinates(0, 0);
+            Vector2 offsets = -Main.screenPosition + offsetZero;
+            Vector2 drawCoords = location + offsets;
+
+            spriteBatch.Draw
+                (
+                    tex, drawCoords, frame, Lighting.GetColor(i, j).MultiplyRGBA(entity.juiceColor), 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f
+                );
+            spriteBatch.End(out var sbSnapshot);
+            spriteBatch.Begin(sbSnapshot);
         }
     }
 }
@@ -75,3 +157,5 @@ public class FruitPunchBowl : ModItem
         Item.value = Item.buyPrice(0, 0, 15, 0);
     }
 }
+
+
