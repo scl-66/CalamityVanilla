9264edf1ea54fdf14f476de956c1e0a102598e21
sixtydegrees <67486775+scl-66@users.noreply.github.com>
2026-06-07 12:29:17 -0400

    Update PrismPiercer.cs

--- Content/Miscellaneous/Items/Weapons/Melee/PrismPiercer/PrismPiercer.cs
+++ Content/Miscellaneous/Items/Weapons/Melee/PrismPiercer/PrismPiercer.cs
@@ -45,7 +45,11 @@ public class PrismPiercer : ModItem
     }
     public override void AddRecipes()
     {
-        CreateRecipe().AddTile(TileID.MythrilAnvil).AddIngredient(ModContent.ItemType<WindowPain.WindowPain>()).AddIngredient(ItemID.Starfury).AddIngredient(ItemID.Ectoplasm, 15).Register();
+        CreateRecipe().AddTile(TileID.MythrilAnvil)
+            .AddIngredient(ModContent.ItemType<WindowPain.WindowPain>())
+            .AddIngredient(ItemID.Starfury)
+            .AddIngredient(ItemID.Ectoplasm, 15)
+            .Register();
     }
     public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
     {
