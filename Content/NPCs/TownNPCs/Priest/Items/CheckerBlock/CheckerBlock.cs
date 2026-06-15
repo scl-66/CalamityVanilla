31114278dc5d97204ea76bec7e955847586f685f
sixtydegrees <67486775+scl-66@users.noreply.github.com>
2026-06-14 22:03:40 -0400

    checker platforms

--- Content/NPCs/TownNPCs/Priest/Items/CheckerBlock/CheckerBlock.cs
+++ Content/NPCs/TownNPCs/Priest/Items/CheckerBlock/CheckerBlock.cs
@@ -39,60 +39,6 @@ public class CheckerBlockTile : ModTile
         return true;
     }
 }
-public class InvertedCheckerBlockTile : ModTile
-{
-    public override string Texture => "CalamityVanilla/Content/NPCs/TownNPCs/Priest/Items/CheckerBlock/CheckerBlockTile";
-
-    public override void SetStaticDefaults()
-    {
-        Main.tileSolid[Type] = true;
-        Main.tileBlockLight[Type] = true;
-        AddMapEntry(new Color(87, 105, 122));
-        HitSound = SoundID.Tink;
-    }
-
-    public override void PostTileFrame(int i, int j, int up, int down, int left, int right, int upLeft, int upRight, int downLeft, int downRight)
-    {
-        // For every even X and Y coordinate, we will offset the tile's horizontal and vertical frame by the size of the sheet so the tile's frame ends up using the alternate version on the sheet, making a pattern that spans 2x2 tiles
-        Tile t = Main.tile[i, j];
-        if ((i + j) % 2 == 0)
-            t.TileFrameY += 90;
-    }
-    public override bool CreateDust(int i, int j, ref int type)
-    {
-        if ((i + j) % 2 == 0)
-        {
-            type = ModContent.DustType<BlackCheckerDust>();
-        }
-        else
-        {
-            type = ModContent.DustType<WhiteCheckerDust>();
-        }
-        return true;
-    }
-}
-internal class CheckerBlock : ModItem
-{
-    public override void SetDefaults()
-    {
-        Item.DefaultToPlaceableTile(ModContent.TileType<CheckerBlockTile>());
-        Item.value = Item.buyPrice(0, 0, 0, 20);
-    }
-    public override void AddRecipes()
-    {
-        CreateRecipe()
-            .AddIngredient<CheckerWall>()
-            .AddTile(TileID.WorkBenches)
-            .Register();
-    }
-}
-internal class InvertedCheckerBlock : ModItem
-{
-    public override void SetDefaults()
-    {
-        Item.DefaultToPlaceableTile(ModContent.TileType<InvertedCheckerBlockTile>());
-    }
-}
 
 //flat color versions
 public class WhiteCheckerBlockTile : ModTile
@@ -131,6 +77,23 @@ public class BlackCheckerBlockTile : ModTile
         t.TileFrameY += 270;
     }
 }
+
+internal class CheckerBlock : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.DefaultToPlaceableTile(ModContent.TileType<CheckerBlockTile>());
+        Item.value = Item.buyPrice(0, 0, 0, 20);
+    }
+    public override void AddRecipes()
+    {
+        CreateRecipe()
+            .AddIngredient<CheckerWall>(4)
+            .AddTile(TileID.WorkBenches)
+            .Register();
+    }
+}
+
 internal class WhiteCheckerBlock : ModItem
 {
     public override void SetDefaults()
@@ -143,6 +106,9 @@ internal class WhiteCheckerBlock : ModItem
         CreateRecipe()
             .AddIngredient<WhiteCheckerWall>()
             .AddTile(TileID.WorkBenches)
+            .Register();
+        CreateRecipe()
+            .AddIngredient<WhiteCheckerPlatform>(2)
             .Register();
     }
 }
@@ -158,6 +124,9 @@ internal class BlackCheckerBlock : ModItem
         CreateRecipe()
             .AddIngredient<BlackCheckerWall>()
             .AddTile(TileID.WorkBenches)
+            .Register();
+        CreateRecipe()
+            .AddIngredient<BlackCheckerPlatform>(2)
             .Register();
     }
 }
---
+++ Content/NPCs/TownNPCs/Priest/Items/CheckerBlock/CheckerPlatform.cs
@@ -0,0 +1,117 @@
+﻿using CalamityVanilla.Content.Dusts;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace CalamityVanilla.Content.NPCs.TownNPCs.Priest.Items.CheckerBlock;
+
+public class WhiteCheckerPlatformTile : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        // Properties
+        Main.tileFrameImportant[Type] = true;
+        Main.tileSolidTop[Type] = true;
+        Main.tileSolid[Type] = true;
+        Main.tileNoAttach[Type] = true;
+        Main.tileTable[Type] = true;
+        Main.tileLavaDeath[Type] = false;
+        TileID.Sets.Platforms[Type] = true;
+        TileID.Sets.DisableSmartCursor[Type] = true;
+
+        AddToArray(ref TileID.Sets.RoomNeeds.CountsAsDoor);
+        AddMapEntry(new Color(197, 200, 199));
+
+        DustType = ModContent.DustType<WhiteCheckerDust>();
+        AdjTiles = [TileID.Platforms];
+        VanillaFallbackOnModDeletion = TileID.Platforms;
+
+        // Placement
+        TileObjectData.newTile.CoordinateHeights = [16];
+        TileObjectData.newTile.CoordinateWidth = 16;
+        TileObjectData.newTile.CoordinatePadding = 2;
+        TileObjectData.newTile.StyleHorizontal = true;
+        TileObjectData.newTile.StyleMultiplier = 27;
+        TileObjectData.newTile.StyleWrapLimit = 27;
+        TileObjectData.newTile.UsesCustomCanPlace = false;
+        TileObjectData.newTile.LavaDeath = false;
+        TileObjectData.addTile(Type);
+    }
+
+    public override void PostSetDefaults() => Main.tileNoSunLight[Type] = false;
+
+    public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
+}
+public class BlackCheckerPlatformTile : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        // Properties
+        Main.tileFrameImportant[Type] = true;
+        Main.tileSolidTop[Type] = true;
+        Main.tileSolid[Type] = true;
+        Main.tileNoAttach[Type] = true;
+        Main.tileTable[Type] = true;
+        Main.tileLavaDeath[Type] = false;
+        TileID.Sets.Platforms[Type] = true;
+        TileID.Sets.DisableSmartCursor[Type] = true;
+
+        AddToArray(ref TileID.Sets.RoomNeeds.CountsAsDoor);
+        AddMapEntry(new Color(97, 105, 122));
+
+        DustType = ModContent.DustType<BlackCheckerDust>();
+        AdjTiles = [TileID.Platforms];
+        VanillaFallbackOnModDeletion = TileID.Platforms;
+
+        // Placement
+        TileObjectData.newTile.CoordinateHeights = [16];
+        TileObjectData.newTile.CoordinateWidth = 16;
+        TileObjectData.newTile.CoordinatePadding = 2;
+        TileObjectData.newTile.StyleHorizontal = true;
+        TileObjectData.newTile.StyleMultiplier = 27;
+        TileObjectData.newTile.StyleWrapLimit = 27;
+        TileObjectData.newTile.UsesCustomCanPlace = false;
+        TileObjectData.newTile.LavaDeath = false;
+        TileObjectData.addTile(Type);
+    }
+
+    public override void PostSetDefaults() => Main.tileNoSunLight[Type] = false;
+
+    public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
+}
+
+internal class WhiteCheckerPlatform : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.DefaultToPlaceableTile(ModContent.TileType<WhiteCheckerPlatformTile>());
+        Item.value = Item.buyPrice(0, 0, 0, 10);
+    }
+    public override void AddRecipes()
+    {
+        CreateRecipe(2)
+            .AddIngredient<WhiteCheckerBlock>()
+            .Register();
+    }
+}
+internal class BlackCheckerPlatform : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.DefaultToPlaceableTile(ModContent.TileType<BlackCheckerPlatformTile>());
+        Item.value = Item.buyPrice(0, 0, 0, 10);
+    }
+    public override void AddRecipes()
+    {
+        CreateRecipe(2)
+            .AddIngredient<BlackCheckerBlock>()
+            .Register();
+    }
+}
